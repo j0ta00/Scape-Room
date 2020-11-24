52b1c4ce546f416f5fbe70211ed87cc9f1a1ef38
AlexRuiiz3 <60139737+AlexRuiiz3@users.noreply.github.com>
2020-11-24 17:42:39 +0100

    Add files via upload

---
+++ Actual/Scape Room/src/test/TestPersonaje.java
@@ -0,0 +1,35 @@
+package test;
+
+import personajes.Personaje;
+
+public class TestPersonaje {
+
+	public static void main(String[] args) {
+		
+		Personaje sinParametros = new Personaje();
+		Personaje conParametros = new Personaje("Prueba",100,10);
+		
+		System.out.println("--- Metodos Fundamentales (GETTERS AND SETTERS) ---");
+		System.out.println("		GETTERS			");
+		System.out.println("sinParametros: Nombre: "+sinParametros.getNombre()+" Danho: "+sinParametros.getDanho()+" Vida: "+sinParametros.getVida());
+		System.out.println("conParametros: Nombre: "+conParametros.getNombre()+" Danho: "+conParametros.getDanho()+" Vida: "+conParametros.getVida());
+		System.out.println("		SETTERS			");
+		conParametros.setVida(-10);
+		System.out.println("conParametros: atributo vida cambia de 100 a 0 --> "+conParametros.getVida());
+		conParametros.setVida(70);
+		System.out.println("conParametros: atributo vida cambia de 0 a 70 --> "+conParametros.getVida());
+		System.out.println("--- Metodos Añadidos ---");
+		System.out.println("		realizarAtaque");
+		int ataque = conParametros.realizarAtaque();
+		System.out.println("conParametros: resultado esperado realizarAtaque 10 o 20 --> "+ataque);
+		System.out.println("		recibirdaño");
+		conParametros.recibirDanho(ataque);
+		System.out.println("conParametros: vida esperada despues del metodo recibirDaño 60 o 50 --> "+conParametros.getVida());
+		conParametros.recibirDanho(100);
+		System.out.println("conParametros: vida esperada despues del metodo recibirDaño 0 --> "+conParametros.getVida());
+		System.out.println("--- Metodos Heredados ---");
+		System.out.println("		toString");
+		System.out.println("conParametros: "+conParametros.toString());
+		
+	}
+}
