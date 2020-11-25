8cadd00f227cb03e9151ff08dde275928a988631
AlexRuiiz3 <alexruiz29del12@gmail.com>
2020-11-25 13:18:08 +0100

    Actual

--- Scape Room/src/principal/Main.java
+++ Scape Room/src/principal/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Scanner teclado = new Scanner(System.in);
+	 Scanner teclado = new Scanner(System.in);
 		
 		//Creacion de los objetos 
 		String pathObjetos = "C:\\Users\\Public\\ObjetosScapeRoom.txt";
